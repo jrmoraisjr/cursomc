ad34834ce0499180cdc32f4960b838c35b6822bd
Jose Roberto <jrmoraisjr@hotmail.com>
2022-06-26 17:55:39 -0300

    Banco de Dados e criacao automatica do banco de dados

--- src/main/java/br/com/jrmoraisjr/cursomc/domain/Categoria.java
+++ src/main/java/br/com/jrmoraisjr/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package br.com.jrmoraisjr.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
