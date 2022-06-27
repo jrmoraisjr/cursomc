b0b9a179f2892b10aea49b2e9909e705e5b7575e
Jose Roberto <jrmoraisjr@hotmail.com>
2022-06-26 22:21:40 -0300

    Produto e associacao muito para Muitos

--- src/main/java/br/com/jrmoraisjr/cursomc/CursomcApplication.java
+++ src/main/java/br/com/jrmoraisjr/cursomc/CursomcApplication.java
@@ -1,13 +1,49 @@
 package br.com.jrmoraisjr.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.jrmoraisjr.cursomc.domain.Categoria;
+import br.com.jrmoraisjr.cursomc.domain.Produto;
+import br.com.jrmoraisjr.cursomc.repositories.CategoriaRepository;
+import br.com.jrmoraisjr.cursomc.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "informatica");
+		Categoria cat2 = new Categoria(null, "escritorio");
+		
+		Produto p1 = new Produto(null, "computador", 2000.00);
+		Produto p2 = new Produto(null, "impressora", 800.00);
+		Produto p3 = new Produto(null, "mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
 	}
 
 }
---
+++ src/main/java/br/com/jrmoraisjr/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package br.com.jrmoraisjr.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.jrmoraisjr.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	
+
+}
